ef35dc9e504ac790a9de530268b977823afa6949
55nuttawad <nxinthcakr@gmail.com>
2024-06-21 13:08:37 +0700

    first commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ export default function Home() {
     <>
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" href="#">Gayhub</Link>
+    <Link className="navbar-brand" href="#">RiHub</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
